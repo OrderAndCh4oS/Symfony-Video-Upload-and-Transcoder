c0da2a6e3872e7cd00c191a372491381b3337010
Sarcoma <sarcoma@live.co.uk>
2018-01-30 11:35:33 +0000

    Fiddled with encoding settings

--- src/Command/AppVideoTrancoderCommand.php
+++ src/Command/AppVideoTrancoderCommand.php
@@ -24,9 +24,27 @@ class AppVideoTrancoderCommand extends Command
     {
         $upload_destination = $input->getArgument('arg1');
         $file = $input->getArgument('arg2');
-        $file_name = pathinfo($file, PATHINFO_FILENAME);
-        $ffmpegCommand = "/usr/bin/ffmpeg -i {$upload_destination}/{$file} -f mp4 -vcodec libx264 -preset slow -profile:v main -acodec aac -strict -2 {$upload_destination}/{$file_name}_newer.mp4 -hide_banner";
+        $ffmpegCommand = $this->ffmpegScript($upload_destination, $file, 1);
         $process = new Process($ffmpegCommand);
         $process->start();
+    }
+
+    /**
+     * @param $upload_destination
+     * @param $file
+     * @return string
+     */
+    private function ffmpegScript($upload_destination, $file, $pass): string
+    {
+        $file_name = pathinfo($file, PATHINFO_FILENAME);
+        $settings = "-vcodec libx264 -preset slow -crf 18 -b:v 3000k -maxrate 4000k -bufsize 512k -c:a aac -b:a 128k -strict -2";
+        $command = "/usr/bin/ffmpeg -y -i {$upload_destination}/{$file} ";
+        $command .= "-f mp4 {$settings} -hide_banner ";
+        $command .= "{$upload_destination}/{$file_name}_new.mp4 -hide_banner";
+        $f = fopen('/var/www/video-uploader/var/ffmpeg.txt', 'w');
+        fwrite($f, $command);
+        fclose($f);
+
+        return $command;
     }
 }
